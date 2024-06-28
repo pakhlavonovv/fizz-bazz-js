af7a9197e32e49431e19c5878770ed62130d517d
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-06-28 08:06:40 +0500

    fizz-buzz

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let son = +prompt("Xoxlagan 3 ga bolinadigon son kiriing")
+if(son / 3){
+    alert("FIZZ")
+} 
+let son2 = +prompt("Xoxlagan 5 ga bolinadigon son kiriing")
+if(son2 / 5){
+    alert("BUZZ")
+}
+let son3 = +prompt("Xoxlagan 5 ga va 3 ga bolinadigon son kiriing")
+if(son2 / 5&&3){
+    alert("FIZZ-BUZZ")
+}
